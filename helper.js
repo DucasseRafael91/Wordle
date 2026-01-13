e517ef076ae3ab7b6ab0ca3b184c15e1cb4def60
Rafael DA SILVA <rafael1893porto@gmail.com>
2026-01-13 14:14:14 +0100

    remove fucking annoying comments

--- helper.js
+++ helper.js
@@ -1,10 +1,8 @@
 function chooseWord(){
     const fs = require("fs");
 
-    // Ler o ficheiro JSON
     const data = JSON.parse(fs.readFileSync("words.json", "utf8"));
 
-    // Escolher uma palavra ao acaso
     const randomWords = data.words[Math.floor(Math.random() * data.words.length)];
 
     return randomWords;
