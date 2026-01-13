82c75b257111270898879f51d2c51b8f695612eb
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-13 14:05:01 +0100

    Correction

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("test")
+// Récupère tous les boutons du clavier
+const buttons = document.querySelectorAll(".key");
+
+// Ajoute un écouteur de clic sur chaque bouton
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        console.log(btn.textContent);
+    });
+});
