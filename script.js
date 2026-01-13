6185dfd539a7197cdce9f80728d18af2650c6a3b
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-13 15:02:27 +0100

    script jeu en cours

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-console.log("test")
-
 const keys = document.querySelectorAll('.key');
+const blocs = document.querySelectorAll('.letter-container');
+
+let step = 0;
+const data = [];
 
 keys.forEach(key => {
-  key.addEventListener('click', e => {
-    console.log(e.target.textContent);
+  key.addEventListener("click", e => {
+    const value = e.target.textContent;
+
+    if (value === "Return") {
+      if (step > 0) {
+        step--;
+        blocs[step].textContent = "";
+        data.pop();
+      }
+    } else {
+      if (step < blocs.length) {
+        blocs[step].textContent = value;
+        data.push(value);
+        step++;
+        console.log(step);
+        if(step  === 5) {
+            console.log('five');
+        }
+      }
+    }
+
+    console.log("data", data);
   });
 });
+
+
